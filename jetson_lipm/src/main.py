b757935521dfac3fda31ee79362895035652c026
aj3944 <aj3944@nyu.edu>
2024-04-01 12:45:05 -0400

    fx

--- jetson_lipm/src/main.py
+++ jetson_lipm/src/main.py
@@ -9,6 +9,11 @@ if __name__ == "__main__":
     print(mark_4.acc)
     print(mark_4.gyro)
     mark_4.readIMU()
+
+    mark_4.swing_hips(5)
+    mark_4.swing_hips(-5)
+
+    mark_4.readIMU()
     print(mark_4.acc)
     print(mark_4.gyro)
     print(mark_4.atilts)
