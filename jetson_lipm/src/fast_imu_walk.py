c034aa5003ed8037635316b7850b6f0545b9ed1d
aj <aj3944@nyu.edu>
2024-04-11 16:20:34 -0400

    fx

--- jetson_lipm/src/fast_imu_walk.py
+++ jetson_lipm/src/fast_imu_walk.py
@@ -13,7 +13,7 @@ mark_4 = Bot()
 mark_4.home()
 time.sleep(3)
 
-sway = 0.5;
+sway = 3.5;
 
 
 
@@ -118,28 +118,60 @@ while not stop:
 			# 	kick_r = -20
 		# if not stable_y:
 				# marignal_toq = copysign(1,gyros[0])
-		if gyros[2] > 0 or gyros[1] < 0:
-		# 	roll_y = 15;
-			print("left leg lift ")
-			left_leg_lift = 0;
-			right_leg_lift = 5;
-			kick_l = 0;
-			step_l = 0;
+		step_len = 5;
+		step_height = 15;
+
+		if gyros[1] > 0:
+			print("stepping back")
+			if gyros[2] < 0 :
+				print("left leg lift ")
+				left_leg_lift = step_height;
+				step_l = step_len;
+			else:
+				print("right leg lift ")
+				right_leg_lift = step_height;
+				step_r = -step_len;
 		else:
-		# 	roll_y = -15;
-			print("right leg lift")
-			left_leg_lift = 5;
-			right_leg_lift = 0;
-			step_r = 0;
-		mark_4.injest_ik_delay([roll_y + step_l - left_leg_lift,2*left_leg_lift,-roll_y - step_r + right_leg_lift,-2*right_leg_lift,kick_l,kick_r],50);
+			print("stepping forward")
+			if gyros[2] > 0 :
+				print("left leg lift ")
+				left_leg_lift = step_height;
+				step_l = -step_len;
+			else:
+				print("right leg lift ")
+				right_leg_lift = step_height;
+				step_r = step_len;
+
+
+		# if gyros[2] > 0 :
+		# # 	roll_y = 15;
+		# 	print("left leg lift ")
+		# 	left_leg_lift = 10;
+		# 	# right_leg_lift = 5;
+		# 	#kick_l = 0;
+		# 	#step_l = 0;
+		# else:
+		# # 	roll_y = -15;
+		# 	print("right leg lift")
+		# 	# left_leg_lift = 5;
+		# 	right_leg_lift = 10;
+		# 	# step_r = 0;
+
+		# if gyros[1] > 0:
+		# 	print("step back")
+		# else:
+		# 	print("step fwd")
+
+
+		mark_4.injest_ik_delay([step_l - left_leg_lift,2*left_leg_lift,- step_r + right_leg_lift,-2*right_leg_lift,kick_l,kick_r],50);
 		# stop = True
-		time.sleep(.1)
-		mark_4.injest_ik_delay([roll_y + step_l + 0 ,0,-0 - 0,0,kick_l,kick_r],100);
-		time.sleep(.1)
+		time.sleep(.2)
+		# mark_4.injest_ik_delay([0 ,0,-0 - 0,0,kick_l,kick_r],200);
+		# time.sleep(1)
 
 
 		mark_4.injest_ik_delay([0,0,0,0,0,0],200);
-		time.sleep(1)			
+		time.sleep(.1)			
 		# if marignal_toq > 0:
 		# 	# Right step 
 		# 	mark_4.injest_ik_delay([roll_y,0,-roll_y,0,kick_l,kick_r],20);
